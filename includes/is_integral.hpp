3f50683d2fb97178642eb3bebab567ebb8bc314b
Nicolas_XPS <nicolasburat1607@gmail.com>
2023-01-19 11:05:47 +0100

    ADD is_integral

--- includes/is_integral.hpp
+++ includes/is_integral.hpp
@@ -1,7 +1,6 @@
 #ifndef IS_INTEGRAL_HPP
 #define IS_INTEGRAL_HPP
 
-#include <type_traits>>
 
 namespace ft {
 
@@ -10,15 +9,53 @@ namespace ft {
 		static const T value = v;
 		typedef T value_type;
 		typedef integral_constant<T, v> type;
-		const operator T() { return v; } 
+		const operator T() const noexcept  { return v; }  // noexcept if for compilation-time to not throw error
 	};
 
-	template <class T>
-	struct is_integral : public integral_constant {
+	typedef ft::integral_constant<bool, true> true_type;
+	typedef ft::integral_constant<bool, false> false_type;
 
+	template<class T>
+	struct is_integral : public false_type{};
 
-	};
+	template <>
+	struct is_integral<bool> : public true_type{};
 
+	template <>
+	struct is_integral<char> : public true_type{};
+
+	template <>
+	struct is_integral<signed char> : public true_type{};
+
+	template <>
+	struct is_integral<unsigned char> : public true_type{};
+
+	template <>
+	struct is_integral<wchar_t> : public true_type{};
+
+	template <>
+	struct is_integral<short> : public true_type{};
+
+	template <>
+	struct is_integral<int> : public true_type{};
+
+	template <>
+	struct is_integral<long> : public true_type{};
+
+	template <>
+	struct is_integral<long long> : public true_type{};
+
+	template <>
+	struct is_integral<unsigned short> : public true_type{};
+
+	template <>
+	struct is_integral<unsigned int> : public true_type{};
+
+	template <>
+	struct is_integral<unsigned long> : public true_type{};
+
+	template <>
+	struct is_integral<unsigned long long> : public true_type{};
 };
 
 #endif
