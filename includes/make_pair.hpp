cbbb16c663fbd023830fcf6281cad0748907c58b
Nicolas_XPS <nicolasburat1607@gmail.com>
2023-01-19 18:42:05 +0100

    ADD make_pair

--- includes/make_pair.hpp
+++ includes/make_pair.hpp
@@ -6,7 +6,7 @@
 namespace ft
 {
 	template<class T1, class T2>
-	inline ft::pair<T1, T2> make_pair (T1 t, T2 u) 
+	inline ft::pair<T1, T2> make_pair(T1 t, T2 u) 
 	{ return ft::pair<T1, T2>(t, u); }	
 }
 
